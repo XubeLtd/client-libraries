4aa09f70646c72b399b280186c6e6d40492fecbb
Francis Saylon <=>
2022-06-08 01:35:09 +0800

    chore: removed sample json files

--- dart/xube_client/lib/xube_client.dart
+++ dart/xube_client/lib/xube_client.dart
@@ -4,20 +4,33 @@ import 'package:flutter/services.dart';
 
 abstract class XubeClient {
   List<Stream> fetchDevicePageStreams(String deviceId);
+  List<Stream> fetchComponentPageStreams(String userId);
 }
 
 class XubeClientImpl implements XubeClient {
   @override
   List<Stream> fetchDevicePageStreams(String deviceId) {
     return [
+      Stream.fromFuture(rootBundle.loadString('examples/device/xube_info.json'))
+          .asBroadcastStream(),
       Stream.fromFuture(
-          rootBundle.loadString('assets/examples/xube_info.json')),
+              rootBundle.loadString('examples/device/xube_endpoint.json'))
+          .asBroadcastStream(),
       Stream.fromFuture(
-          rootBundle.loadString('assets/examples/xube_endpoint.json')),
+              rootBundle.loadString('examples/device/xube_connectivity.json'))
+          .asBroadcastStream(),
       Stream.fromFuture(
-          rootBundle.loadString('assets/examples/xube_connectivity.json')),
+              rootBundle.loadString('examples/device/xube_power.json'))
+          .asBroadcastStream(),
+    ];
+  }
+
+  @override
+  List<Stream> fetchComponentPageStreams(String userId) {
+    return [
       Stream.fromFuture(
-          rootBundle.loadString('assets/examples/xube_power.json')),
+              rootBundle.loadString('examples/components/xube_components.json'))
+          .asBroadcastStream(),
     ];
   }
 }
