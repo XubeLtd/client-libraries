a9ce83da2f79d86c61fa7e3a49dc6381d170f52c
Francis Saylon <28182748+ftsaylon@users.noreply.github.com>
2022-07-01 18:40:48 +0800

    chore: added assets/ path in components

--- dart/xube_client/lib/src/component.dart
+++ dart/xube_client/lib/src/component.dart
@@ -3,8 +3,8 @@ import 'package:flutter/services.dart';
 class XubeClientComponent {
   List<Stream> fetchComponentPageStreams(String userId) {
     return [
-      Stream.fromFuture(
-              rootBundle.loadString('examples/components/xube_components.json'))
+      Stream.fromFuture(rootBundle
+              .loadString('assets/examples/components/xube_components.json'))
           .asBroadcastStream(),
     ];
   }
