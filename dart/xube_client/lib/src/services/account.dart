5e149723162fa0a7b525a827c05bfd023106e0c5
Francis Saylon <28182748+ftsaylon@users.noreply.github.com>
2022-08-31 02:18:14 +0800

    chore: implemented getAccountStream

--- dart/xube_client/lib/src/services/account.dart
+++ dart/xube_client/lib/src/services/account.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/services.dart';
+import 'dart:developer';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:xube_client/src/utils/subcscription_manager.dart';
 import 'package:xube_client/xube_client.dart';
+import 'dart:convert';
 
 class XubeClientAccount {
   final WebSocketChannel _channel;
@@ -17,9 +18,29 @@ class XubeClientAccount {
         _subscriptionManager =
             subscriptionManager ?? SubscriptionManager.instance;
 
-  Stream getAccountStream(String userId) {
-    return Stream.fromFuture(
-            rootBundle.loadString('assets/examples/account/account.json'))
-        .asBroadcastStream();
+  Stream? getAccountStream(String accountId) {
+    if (!_auth.isAuth || _auth.userId == null || _auth.email == null) {
+      return null;
+    }
+
+    var stream = _subscriptionManager.findStreamById(accountId);
+    if (stream != null) return stream;
+
+    log('getUserAccountsStream: subscribing to play@xube.io');
+    _channel.sink.add(
+      json.encode({
+        "action": "Subscribe",
+        "format": "View",
+        "contextType": "ACCOUNT",
+        "subscriptionType": "ACCOUNT",
+        "subscriptionID": accountId,
+      }),
+    );
+
+    _subscriptionManager.createSubscription(accountId);
+    stream = _subscriptionManager.findStreamById(accountId);
+
+    log('getAccountStream: $stream');
+    return stream;
   }
 }
